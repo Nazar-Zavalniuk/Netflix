f79d7c0ad8d0cdad8c3946c6c9de34d55a04dbf8
Zavalniuk-Nazar <nazarzaval99@gmail.com>
2024-06-25 15:40:38 +0300

    Moved the background image from "GetStartedSection" to "Vercel Blob Storage" and updated the "Background" component and "NextJS" configuration to load this image.

--- app/(homepage)/components/get-started-section/components/background/background.tsx
+++ app/(homepage)/components/get-started-section/components/background/background.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
 
 import styles from "./background.module.css";
-import BgImage from "./images/get-started-background.jpg";
 
 export function Background() {
   return (
     <div className="w-full h-full absolute left-0 top-0">
       <div className="w-full h-full overflow-hidden relative">
         <Image
-          src={BgImage}
+          src={
+            "https://vimsxaud7lt1te1j.public.blob.vercel-storage.com/get-started-background-FrPzUvXkIh9IBszIQn32eEGEwtOHWA.jpg"
+          }
           alt=""
           priority
-          sizes="100vw"
+          fill
           aria-hidden={true}
           className="w-auto sm:w-full h-full sm:scale-125 sm:translate-y-[-10%] object-cover"
         />
