21f956dd3b7b91e8fcadd6157ddababa0ac74abe
Zavalniuk-Nazar <nazarzaval99@gmail.com>
2024-08-03 13:18:36 +0300

    Prepared a preliminary data structure for the films.

---
+++ common/data/data.ts
@@ -0,0 +1,122 @@
+/* eslint-disable sort-keys */
+
+const DATA = [
+  // SERIES
+
+  {
+    title: "Ginny & Georgia",
+    preview:
+      "https://vimsxaud7lt1te1j.public.blob.vercel-storage.com/preview-G&G-en-vY9w3ydV7Dw15hqxnAWBqF91z3HCQc.jpg",
+    coverImage:
+      "https://vimsxaud7lt1te1j.public.blob.vercel-storage.com/CI-G&G-28RjsnoHrRNmaIAg4TAl8pN4asn7Y1.jpg",
+    trailer: "https://www.youtube.com/watch?v=8eNlumgnhu0",
+    releaseYear: 2021,
+    synopsis:
+      "Free-spirited Georgia and her two kids, Ginny and Austin, move north in search of a fresh start but find that the road to new beginnings can be bumpy.",
+    type: "Series",
+    duration: "2 Seasons",
+    authors: ["Sarah Lampert"],
+    cast: [
+      "Brianne Howey",
+      "Antonia Gentry",
+      "Diesel La Torraca",
+      "Jennifer Robertson",
+      "Felix Mallard",
+    ],
+    coreGenre: "Drama",
+    genres: [
+      "Romantic TV Dramas",
+      "TV Dramas",
+      "Teen TV Shows",
+      "TV Comedies",
+      "Romantic TV Comedies",
+    ],
+    vibes: ["Scandalous", "Emotional"],
+    ageLimit: 16,
+    contentWarnings: "sex, language, substances, self-harm",
+    seasons: [
+      {
+        seasonNumber: 1,
+        releaseYear: 2021,
+        ageLimit: 16,
+        contentWarnings: "self-harm",
+        episodes: [
+          {
+            episodeNumber: 1,
+            title: "Pilot",
+            preview:
+              "https://vimsxaud7lt1te1j.public.blob.vercel-storage.com/s1-e1-G&G-FfI6swbzIk0DBD7IYMCbS9vwzALWCq.jpg",
+            synopsis:
+              "Georgia Miller arrives in the quaint New England town of Wellsbury with her daughter, Ginny, and her son, Austin, and quickly makes a big impression.",
+            duration: "58m",
+          },
+          {
+            episodeNumber: 2,
+            title: "It's a Face Not a Mask",
+            preview:
+              "https://vimsxaud7lt1te1j.public.blob.vercel-storage.com/s1-e2-G&G.-jCDW6gtXigeTeOWB9XUm90QQo5Tu2Q.jpg",
+            synopsis:
+              "As Ginny struggles to fit in with her new friends, she faces fallout from a night with Marcus. Georgia helps problem-solve for both Paul and Ellen.",
+            duration: "50m",
+          },
+          {
+            episodeNumber: 3,
+            title: "Next Level Rich People Sh*t",
+            preview:
+              "https://vimsxaud7lt1te1j.public.blob.vercel-storage.com/s1-e3-G&G-kzhmWy031zpDvwLCgnaL0XvcrQDMId.jpg",
+            synopsis:
+              "During the Sophomore Sleepover, Max bares her heart, Georgia resorts to old, bad habits, and Ginny shares connections with both Hunter and Marcus.",
+            duration: "50m",
+          },
+          {
+            episodeNumber: 4,
+            title: "Lydia Bennet Is Hundo a Feminist",
+            preview:
+              "https://vimsxaud7lt1te1j.public.blob.vercel-storage.com/s1-e4-G&G-vh58f5aK7Pfy6qcDQ1aJbcyk1JMnyy.jpg",
+            synopsis:
+              "Hunter's aloofness makes Ginny insecure. Max obsesses over a new love interest. Georgia sells Paul on an idea for a fundraiser — then has to deliver.",
+            duration: "51m",
+          },
+        ],
+      },
+    ],
+    completed: false,
+  },
+
+  // MOVIES
+
+  {
+    title: "Jumanji",
+    preview:
+      "https://vimsxaud7lt1te1j.public.blob.vercel-storage.com/preview-jumanji-en-BgmO9cSBKOV3MYDMuu4ldSHpSNuJXq.webp",
+    coverImage:
+      "https://vimsxaud7lt1te1j.public.blob.vercel-storage.com/CI-Jumanji-jAfI5SMRY63eSu7FNTvvVOdjDF2xJh.jpg",
+    trailer:
+      "https://www.youtube.com/watch?v=2QKg5SZ_35I&ab_channel=SonyPicturesEntertainment",
+    releaseYear: 2019,
+    synopsis:
+      "When Spencer goes missing, Martha, Bethany and Fridge realize they must go back into Jumanji to find him — but something goes wrong.",
+    type: "Movie",
+    duration: "2h 3m",
+    authors: ["Jake Kasdan"],
+    cast: [
+      "Dwayne Johnson",
+      "Jack Black",
+      "Kevin Hart",
+      "Karen Gillan",
+      "Nick Jonas",
+    ],
+    coreGenre: "Fantasy",
+    genres: [
+      "Family Movies",
+      "Comedy Movies",
+      "Movies Based on Books",
+      "Action & Adventure Movies",
+    ],
+    vibes: ["Adrenaline Rush", "Imaginative", "Exciting"],
+    ageLimit: 13,
+    contentWarnings: "violence",
+    seasons: null,
+    completed: true,
+  },
+];
